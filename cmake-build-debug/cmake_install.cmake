ef8ed15193988c19c57b87b5012b9ee4d142edca
Schnizel14 <lorenz.kasieczka@gmail.com>
2022-04-26 14:05:14 +0200

    uzzogzgkukgvghgcfgggfgfg

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/loren/Desktop/Schule/C/Linkt_List/Krenmayr-Kasieczka-LinkedList-2022
+# Install script for directory: C:/Users/DI Paul Krenmayr/Desktop/Schule/PRPR/Krenmayr-Kasieczka-LinkedList-2022
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files (x86)/mingw-w64/i686-8.1.0-posix-dwarf-rt_v6-rev0/mingw32/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/DI Paul Krenmayr/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5080.224/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/loren/Desktop/Schule/C/Linkt_List/Krenmayr-Kasieczka-LinkedList-2022/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/DI Paul Krenmayr/Desktop/Schule/PRPR/Krenmayr-Kasieczka-LinkedList-2022/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <malloc.h>
 
+typedef struct node {
+    int data;
+    struct node * next;
+} Node;
+
+Node *head = NULL;
 int main() {
-    printf("Hello, World!\n");
+
+
+    head = (Node *) malloc(sizeof(Node));
+    if (head == NULL) {
+        return 1;
+    }
+
+    head->data = 1;
+    head->next = NULL;
     return 0;
+}
+void printList() {
+    Node *current = head;
+
+    while(current != NULL){
+        printf()
+    }
 }
