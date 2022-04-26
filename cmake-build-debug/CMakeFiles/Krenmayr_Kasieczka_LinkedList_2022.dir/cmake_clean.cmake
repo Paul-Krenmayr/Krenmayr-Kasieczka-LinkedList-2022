d492752be8c2c40a8fb30ba44e97b3fd6b8fd800
Paul-Krenmayr <paul.krenmayr@gmail.com>
2022-04-26 14:04:45 +0200

    code!?

---
+++ cmake-build-debug/CMakeFiles/Krenmayr_Kasieczka_LinkedList_2022.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Krenmayr_Kasieczka_LinkedList_2022.dir/main.c.obj"
+  "CMakeFiles/Krenmayr_Kasieczka_LinkedList_2022.dir/main.c.obj.d"
+  "Krenmayr_Kasieczka_LinkedList_2022.exe"
+  "Krenmayr_Kasieczka_LinkedList_2022.exe.manifest"
+  "Krenmayr_Kasieczka_LinkedList_2022.pdb"
+  "libKrenmayr_Kasieczka_LinkedList_2022.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Krenmayr_Kasieczka_LinkedList_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Krenmayr_Kasieczka_LinkedList_2022.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for Krenmayr_Kasieczka_LinkedList_2022.
